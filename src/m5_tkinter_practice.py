18fa5962fad1571da7878f4579e26461b257894b
JrOtaku <emilya200.ea@gmail.com>
2019-01-31 14:10:34 -0500

    done

--- src/m5_tkinter_practice.py
+++ src/m5_tkinter_practice.py
@@ -3,7 +3,7 @@ This project lets you try out Tkinter/Ttk and practice it!
 
 Authors: David Mutchler, Vibha Alangar, Matt Boutell, Dave Fisher,
          Mark Hays, Amanda Stouder, Aaron Wilkin, their colleagues,
-         and PUT_YOUR_NAME_HERE.
+         and Emily Guajardo.
 """  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 import tkinter
@@ -16,22 +16,32 @@ def main():
     # TODO: 2. After reading and understanding the m1e module,
     #   ** make a window that shows up. **
     # -------------------------------------------------------------------------
+    window = tkinter.Tk()
+
 
     # -------------------------------------------------------------------------
     # TODO: 3. After reading and understanding the m2e module,
     #   ** put a Frame on the window. **
     # -------------------------------------------------------------------------
 
+    frame1 = ttk.Frame(window, padding=20)
+    frame1.grid()
+
     # -------------------------------------------------------------------------
     # TODO: 4. After reading and understanding the m2e module,
     #   ** put a Button on the Frame. **
     # -------------------------------------------------------------------------
+
+    # hello_button = ttk.Button(frame1, text='Print Hello')
+    # hello_button.grid()
 
     # -------------------------------------------------------------------------
     # TODO: 5. After reading and understanding the m3e module,
     #   ** make your Button respond to a button-press **
     #   ** by printing   "Hello"  on the Console.     **
     # -------------------------------------------------------------------------
+
+    # hello_button['command'] = lambda: print('Hello')
 
     # -------------------------------------------------------------------------
     # TODO: 6. After reading and understanding the m4e module,
@@ -41,6 +51,13 @@ def main():
     #        on the Console if the current string in the Entry box
     #        is the string 'ok', but print "Goodbye" otherwise.
     # -------------------------------------------------------------------------
+
+    button = ttk.Button(frame1, text="Enter ok")
+    entry = ttk.Entry(frame1)
+    button.grid()
+    entry.grid()
+    button['command'] = lambda: is_entry_ok(entry)
+
 
     # -------------------------------------------------------------------------
     # TODO: 7.
@@ -69,8 +86,14 @@ def main():
     # -------------------------------------------------------------------------
     # TODO: 8. As time permits, do other interesting GUI things!
     # -------------------------------------------------------------------------
+    window.mainloop()
 
 
+def is_entry_ok(text):
+    if text.get() == 'ok':
+        print('Hello')
+    else:
+        print('Goodbye')
 # -----------------------------------------------------------------------------
 # Calls  main  to start the ball rolling.
 # -----------------------------------------------------------------------------
